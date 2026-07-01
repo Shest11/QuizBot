bf75de1bc78f84df7119cba3476462402d9d1d9a
ShestM <danilasestakov8@gmail.com>
2026-07-01 21:09:05 +0800

    redirect root main.py to src/bot/main.py launcher

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from quizbot!")
+import asyncio
+import sys
+from pathlib import Path
 
+sys.path.insert(0, str(Path(__file__).parent / "src")) # src становится путем для импорта, чтобы перейти в bot.main
+
+from bot.main import main as run_bot
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(run_bot())
